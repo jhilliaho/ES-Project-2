ba9744ab03b4fefded2fc09b8663ba406b2a86cc
Sebastian Rehfeldt <Sebastian.Rehfeldt@gmx.net>
2017-03-09 17:26:53 +0000

    linking to client

--- server/server.py
+++ server/server.py
@@ -2,15 +2,20 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask_cors import CORS, cross_origin
+from flask import render_template
 import json
 import os
 
-app = Flask(__name__)
+template_dir = os.path.abspath('../client/build')
+static_dir = os.path.abspath('../client/build/static')
+app = Flask(__name__,template_folder=template_dir,static_folder=static_dir)
 CORS(app)
 
 @app.route('/', methods=["GET"])
 def list():
-	return "Hello world!"
+	return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(host="localhost", port=3001)
