cd7fd69b27bd6f3dc96235277c3ee7f0daa1502c
Jani Hilliaho <jani.hilliaho@gmail.com>
2017-03-23 18:10:06 +0000

    Added configuration example

---
+++ server/configuration_example.py
@@ -0,0 +1,7 @@
+# THIS FILE NEEDS TO BE COPIED TO "configuration.py"
+
+db_host = "localhost"
+db_name = "db_name"
+username = "user_name"
+password = "nanananaaa"
+secret_key = "very secret phrase9340u4390jg"
